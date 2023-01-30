ae426e4c455eeaf5b70240ab52db19350ed21282
sivaprasadnk <sivaprasadnk123@gmail.com>
2023-01-30 15:25:33 +0530

    workflow  changes

--- lib/views/splash.screen.dart
+++ lib/views/splash.screen.dart
@@ -27,7 +27,6 @@ class _SplashScreenState extends State<SplashScreen> {
     InAppUpdate.checkForUpdate().then((info) {
       if (info.updateAvailability ==
           UpdateAvailability.updateAvailable) {
-        debugPrint('here');
         InAppUpdate.performImmediateUpdate()
             .catchError((e) => showSnack(e.toString()));
       } else {
